4e7f98f4db00d390c6e98bc4e8c3f70e4a164e1e
IceColdCode <IceCodeDev@gmail.com>
2015-05-29 11:32:56 -0600

    Update GameImage.java

--- src/TileMap/GameImage.java
+++ src/TileMap/GameImage.java
@@ -1,1 +1,21 @@
+package TileMap;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.event.*;
+
+abstract class GameImage {
+  
+  // positions on the screen
+  private int x;
+  private int y;
+  private int dx;
+  private int dy;
+  
+  // image
+  BufferedImage image;
+  
+  public void loadImage(String file);
+  
+  public void move();
+}
