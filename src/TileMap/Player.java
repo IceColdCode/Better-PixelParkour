69005fc497d2578be72545d0563d5b7a755c1553
IceColdCode <IceCodeDev@gmail.com>
2015-06-01 15:17:06 -0600

    Update Player.java

--- src/TileMap/Player.java
+++ src/TileMap/Player.java
@@ -5,6 +5,18 @@ import TileMap.Sprite;
 
 public class Player {
   
+  // positioning
+  public int x;
+  public int y;
+  public int dx;
+  public int dy;
   
+  public void jump() {
+    
+    int timesOperated = 0;
+    
+    while(timesOperated != 0)
+    
+  }
   
 }
